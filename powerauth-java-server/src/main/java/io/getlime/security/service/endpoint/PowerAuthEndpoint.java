fe2095d51e7ffd9760a3acffedd27bd5fba92542
Petr Dvorak <petr@inmite.eu>
2015-11-08 21:21:20 +0100

    Fixed PowerAuth 2.0 Web-Service Endpoint Stub

--- powerauth-java-server/src/main/java/io/getlime/security/service/endpoint/PowerAuthEndpoint.java
+++ powerauth-java-server/src/main/java/io/getlime/security/service/endpoint/PowerAuthEndpoint.java
@@ -1,8 +1,24 @@
 package io.getlime.security.service.endpoint;
 
+import io.getlime.security.powerauth.BlockActivationRequest;
+import io.getlime.security.powerauth.BlockActivationResponse;
+import io.getlime.security.powerauth.CommitActivationRequest;
+import io.getlime.security.powerauth.CommitActivationResponse;
+import io.getlime.security.powerauth.GetActivationListForUserRequest;
+import io.getlime.security.powerauth.GetActivationListForUserResponse;
+import io.getlime.security.powerauth.GetActivationStatusRequest;
+import io.getlime.security.powerauth.GetActivationStatusResponse;
 import io.getlime.security.repository.PowerAuthRepository;
 import io.getlime.security.powerauth.InitActivationRequest;
 import io.getlime.security.powerauth.InitActivationResponse;
+import io.getlime.security.powerauth.PrepareActivationRequest;
+import io.getlime.security.powerauth.PrepareActivationResponse;
+import io.getlime.security.powerauth.RemoveActivationRequest;
+import io.getlime.security.powerauth.RemoveActivationResponse;
+import io.getlime.security.powerauth.UnblockActivationRequest;
+import io.getlime.security.powerauth.UnblockActivationResponse;
+import io.getlime.security.powerauth.VerifySignatureRequest;
+import io.getlime.security.powerauth.VerifySignatureResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -21,13 +37,69 @@ public class PowerAuthEndpoint {
         this.powerAuthRepository = powerAuthRepository;
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "initActivationRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "InitActivationRequest")
     @ResponsePayload
     public InitActivationResponse initActivation(@RequestPayload InitActivationRequest request) {
         InitActivationResponse response = new InitActivationResponse();
         response.setActivationIdShort("10");
-        response.setActivationIdShort("20");
-        response.setActivationIdShort("30");
+        response.setActivationOTP("20");
+        response.setActivationSignature("30");
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "PrepareActivationRequest")
+    @ResponsePayload
+    public PrepareActivationResponse prepareActivation(@RequestPayload PrepareActivationRequest request) {
+        PrepareActivationResponse response = new PrepareActivationResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "CommitActivationRequest")
+    @ResponsePayload
+    public CommitActivationResponse commitActivation(@RequestPayload CommitActivationRequest request) {
+        CommitActivationResponse response = new CommitActivationResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetActivationStatusRequest")
+    @ResponsePayload
+    public GetActivationStatusResponse getActivationStatus(@RequestPayload GetActivationStatusRequest request) {
+        GetActivationStatusResponse response = new GetActivationStatusResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "RemoveActivationRequest")
+    @ResponsePayload
+    public RemoveActivationResponse removeActivation(@RequestPayload RemoveActivationRequest request) {
+        RemoveActivationResponse response = new RemoveActivationResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetActivationListForUserRequest")
+    @ResponsePayload
+    public GetActivationListForUserResponse getActivatioListForUser(@RequestPayload GetActivationListForUserRequest request) {
+        GetActivationListForUserResponse response = new GetActivationListForUserResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "VerifySignatureRequest")
+    @ResponsePayload
+    public VerifySignatureResponse verifySignature(@RequestPayload VerifySignatureRequest request) {
+        VerifySignatureResponse response = new VerifySignatureResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "BlockActivationRequest")
+    @ResponsePayload
+    public BlockActivationResponse blockActivation(@RequestPayload BlockActivationRequest request) {
+        BlockActivationResponse response = new BlockActivationResponse();
+        return response;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "UnblockActivationRequest")
+    @ResponsePayload
+    public UnblockActivationResponse unblockActivation(@RequestPayload UnblockActivationRequest request) {
+        UnblockActivationResponse response = new UnblockActivationResponse();
         return response;
     }
 
