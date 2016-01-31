3584a0662f69dd7c90deeb1a3cda331ef2e78cf0
Petr Dvorak <petr@inmite.eu>
2016-01-31 22:02:19 +0100

    Fix #43: Consider renaming the default SOAP interface to something else than 'powerauth'
    Fixed by renaming the /powerauth SOAP interface to /soap and adding /rest for the RESTful endpoints

--- powerauth-java-server/src/main/java/io/getlime/security/service/controller/PowerAuthController.java
+++ powerauth-java-server/src/main/java/io/getlime/security/service/controller/PowerAuthController.java
@@ -47,63 +47,63 @@ import io.getlime.security.powerauth.VerifySignatureResponse;
 import io.getlime.security.service.PowerAuthService;
 
 @Controller
-@RequestMapping(value = "/pa")
+@RequestMapping(value = "/rest/pa")
 public class PowerAuthController {
 
     @Autowired
     private PowerAuthService powerAuthService;
 
-    @RequestMapping(value = "activation/init", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/init", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<InitActivationResponse> initActivation(@RequestBody RESTRequestWrapper<InitActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.initActivation(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/prepare", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/prepare", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<PrepareActivationResponse> prepareActivation(@RequestBody RESTRequestWrapper<PrepareActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.prepareActivation(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/commit", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/commit", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<CommitActivationResponse> commitActivation(@RequestBody RESTRequestWrapper<CommitActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.commitActivation(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/status", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/status", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<GetActivationStatusResponse> getActivationStatus(@RequestBody RESTRequestWrapper<GetActivationStatusRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.getActivationStatus(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/remove", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/remove", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<RemoveActivationResponse> removeActivation(@RequestBody RESTRequestWrapper<RemoveActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.removeActivation(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/list", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/list", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<GetActivationListForUserResponse> getActivatioListForUser(@RequestBody RESTRequestWrapper<GetActivationListForUserRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.getActivatioListForUser(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "signature/verify", method = RequestMethod.POST)
+    @RequestMapping(value = "/signature/verify", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<VerifySignatureResponse> verifySignature(@RequestBody RESTRequestWrapper<VerifySignatureRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.verifySignature(request.getRequestObject()));
     }
     
-    @RequestMapping(value = "signature/list", method = RequestMethod.POST)
+    @RequestMapping(value = "/signature/list", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<SignatureAuditResponse> getSignatureAuditLog(@RequestBody RESTRequestWrapper<SignatureAuditRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.getSignatureAuditLog(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/block", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/block", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<BlockActivationResponse> blockActivation(@RequestBody RESTRequestWrapper<BlockActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.blockActivation(request.getRequestObject()));
     }
 
-    @RequestMapping(value = "activation/unblock", method = RequestMethod.POST)
+    @RequestMapping(value = "/activation/unblock", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<UnblockActivationResponse> unblockActivation(@RequestBody RESTRequestWrapper<UnblockActivationRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.unblockActivation(request.getRequestObject()));
     }
     
-    @RequestMapping(value = "vault/unlock", method = RequestMethod.POST)
+    @RequestMapping(value = "/vault/unlock", method = RequestMethod.POST)
     public @ResponseBody RESTResponseWrapper<VaultUnlockResponse> vaultUnlock(@RequestBody RESTRequestWrapper<VaultUnlockRequest> request) throws Exception {
         return new RESTResponseWrapper<>("OK", powerAuthService.vaultUnlock(request.getRequestObject()));
     }
