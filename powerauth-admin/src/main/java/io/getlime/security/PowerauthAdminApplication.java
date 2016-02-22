79b229d8ddb36e78aac7c901a14f8d687391b013
Petr Dvorak <petr@inmite.eu>
2016-02-22 14:37:20 -0800

    Add a maven build step to copy the build artifacts in the 'dist' folder on 'package' phase

---
+++ powerauth-admin/src/main/java/io/getlime/security/PowerauthAdminApplication.java
@@ -0,0 +1,12 @@
+package io.getlime.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerauthAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerauthAdminApplication.class, args);
+	}
+}
---
+++ powerauth-admin/src/main/java/io/getlime/security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.getlime.security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PowerauthAdminApplication.class);
+	}
+
+}
