aefb43524f3905d455e0904c3b667cdf671ad3a5
Petr Dvorak <petr@inmite.eu>
2016-03-14 14:29:21 +0100

    [minor] Fixed file formatting

--- powerauth-java/src/main/java/io/getlime/security/powerauth/lib/enums/PowerAuthSignatureTypes.java
+++ powerauth-java/src/main/java/io/getlime/security/powerauth/lib/enums/PowerAuthSignatureTypes.java
@@ -19,43 +19,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum PowerAuthSignatureTypes {
-	
+
 	POSSESSION("possession"),
-	KNOWLEDGE("knowledge"),
-	BIOMETRY("biometry"),
-	POSSESSION_KNOWLEDGE("possession_knowledge"),
-	POSSESSION_BIOMETRY("possession_biometry"),
+	KNOWLEDGE("knowledge"), 
+	BIOMETRY("biometry"), 
+	POSSESSION_KNOWLEDGE("possession_knowledge"), 
+	POSSESSION_BIOMETRY("possession_biometry"), 
 	POSSESSION_KNOWLEDGE_BIOMETRY("possession_knowledge_biometry");
-	
+
 	private String value;
-	
+
 	private static Map<String, PowerAuthSignatureTypes> map = new HashMap<>();
 
-    static {
-        for (PowerAuthSignatureTypes type : PowerAuthSignatureTypes.values()) {
-            map.put(type.value, type);
-        }
-    }
-    
-    private PowerAuthSignatureTypes(final String value) {
-    	this.value = value;
-    }
+	static {
+		for (PowerAuthSignatureTypes type : PowerAuthSignatureTypes.values()) {
+			map.put(type.value, type);
+		}
+	}
 
-    public static PowerAuthSignatureTypes getEnumFromString(String value) {
-    	PowerAuthSignatureTypes type = map.get(value);
-    	if (type == null) { // try to guess the most usual suspect...
-    		return PowerAuthSignatureTypes.POSSESSION_KNOWLEDGE;
-    	} else {
-    		return type;
-    	}
-    }
+	private PowerAuthSignatureTypes(final String value) {
+		this.value = value;
+	}
 
-    public boolean equalsName(String otherName) {
-        return (otherName == null) ? false : value.equals(otherName);
-    }
-    
-    public String toString() {
-        return this.value;
-     }
+	public static PowerAuthSignatureTypes getEnumFromString(String value) {
+		PowerAuthSignatureTypes type = map.get(value);
+		if (type == null) { // try to guess the most usual suspect...
+			return PowerAuthSignatureTypes.POSSESSION_KNOWLEDGE;
+		} else {
+			return type;
+		}
+	}
+
+	public boolean equalsName(String otherName) {
+		return (otherName == null) ? false : value.equals(otherName);
+	}
+
+	public String toString() {
+		return this.value;
+	}
 
 }
