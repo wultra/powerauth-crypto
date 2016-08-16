d65c5449b2147f0fa837e5b347df30eb262fa261
Miroslav Michalec <michalec.miroslav@gmail.com>
2016-08-16 10:54:38 +0200

    Added TrustManager that allows connection to servers with unsecured self-signed certificates

--- powerauth-java-cmd/src/main/java/io/getlime/security/client/app/Application.java
+++ powerauth-java-cmd/src/main/java/io/getlime/security/client/app/Application.java
@@ -22,9 +22,7 @@ import java.security.Security;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
+import javax.net.ssl.*;
 
 import io.getlime.security.powerauth.lib.provider.CryptoProviderUtilFactory;
 import org.apache.commons.cli.CommandLine;
@@ -110,6 +108,25 @@ public class Application implements CommandLineRunner {
 						return true;
 					}
 				});
+
+				TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+
+					public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+					}
+
+					public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+					}
+				}};
+
+				try {
+					SSLContext sc = SSLContext.getInstance("SSL");
+					sc.init(null, trustAllCerts, new java.security.SecureRandom());
+					HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+				} catch (Exception e) {
+				}
 			}
 
 			// Read values
