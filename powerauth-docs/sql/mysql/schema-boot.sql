84322731fa7e74c0e28ed0ff961e54c4fa84271a
Petr Dvorak <petr@inmite.eu>
2017-03-17 17:26:59 +0100

    Fix file name

--- powerauth-docs/sql/mysql/schema-boot.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA `powerauth` DEFAULT CHARACTER SET utf8 ;
-
-CREATE USER 'powerauth'@'localhost';
-
-GRANT ALL PRIVILEGES ON powerauth.* TO 'powerauth'@'localhost';
-
-FLUSH PRIVILEGES;
