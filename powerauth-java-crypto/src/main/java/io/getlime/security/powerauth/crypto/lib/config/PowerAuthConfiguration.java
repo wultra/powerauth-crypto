d14cc227a66f6308dc221482fab7812eba01cf82
Petr Dvorak <petr@inmite.eu>
2017-09-10 13:39:28 +0200

    Fix #105: Isolate configuration of the server from configuration of the crypto algorithm Update library versions

--- powerauth-java-crypto/src/main/java/io/getlime/security/powerauth/crypto/lib/config/PowerAuthConfiguration.java
+++ powerauth-java-crypto/src/main/java/io/getlime/security/powerauth/crypto/lib/config/PowerAuthConfiguration.java
@@ -60,24 +60,6 @@ public enum PowerAuthConfiguration {
     public static final int PBKDF_ITERATIONS = 10000;
 
     /**
-     * When a duplicate activation ID is encountered during the activation, how
-     * many times generate a new one.
-     */
-    public static final int ACTIVATION_GENERATE_ACTIVATION_ID_ITERATIONS = 10;
-
-    /**
-     * When a duplicate activation short ID is encountered during the
-     * activation, how many times generate a new one.
-     */
-    public static final int ACTIVATION_GENERATE_ACTIVATION_SHORT_ID_ITERATIONS = 10;
-
-    /**
-     * How many milliseconds should be CREATED or OTP_USED record usable for
-     * completing the activation.
-     */
-    public static final int ACTIVATION_VALIDITY_BEFORE_ACTIVE = 2 * 60 * 1000;
-
-    /**
      * Length of device public key fingerprint.
      */
     public static final int FINGERPRINT_LENGTH = 8;
@@ -86,15 +68,5 @@ public enum PowerAuthConfiguration {
      * Signature length (number of decimal numbers representing signature)
      */
     public static final int SIGNATURE_LENGTH = 8;
-
-    /**
-     * How many failed signatures cause activation record blocking
-     */
-    public static final long SIGNATURE_MAX_FAILED_ATTEMPTS = 5L;
-
-    /**
-     * When validating the signature, how many iterations ahead too look
-     */
-    public static final long SIGNATURE_VALIDATION_LOOKAHEAD = 20L;
 
 }
