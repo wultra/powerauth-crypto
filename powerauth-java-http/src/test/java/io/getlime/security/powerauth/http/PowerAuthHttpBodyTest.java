8b3e777e689f30884a7b84767c45ce603d3b792c
Petr Dvorak <petr@wultra.com>
2018-08-30 12:33:14 +0200

    Fix copyrights

--- powerauth-java-http/src/test/java/io/getlime/security/powerauth/http/PowerAuthHttpBodyTest.java
+++ powerauth-java-http/src/test/java/io/getlime/security/powerauth/http/PowerAuthHttpBodyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Lime - HighTech Solutions s.r.o.
+ * Copyright 2016 Wultra s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import static org.junit.Assert.*;
 /**
  * Tests for HTTP body normalization routine.
  *
- * @author Petr Dvorak, petr@lime-company.eu
+ * @author Petr Dvorak, petr@wultra.com
  */
 public class PowerAuthHttpBodyTest {
 
