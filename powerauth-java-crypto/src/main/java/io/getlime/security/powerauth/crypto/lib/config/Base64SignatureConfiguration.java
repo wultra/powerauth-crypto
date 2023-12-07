0a1cdca27c441de8475e33fd5d249cfb63d5f72b
Jan Dusil <134381434+jandusil@users.noreply.github.com>
2023-12-07 12:22:38 +0100

    Fix #451: Allow overriding length of decimalized signatures (#452) (#553)
    * Fix #451: Allow overriding length of decimalized singatures
    - Change code based on code review
    - Fix test vector
    - Fix JavaDoc
    - Add invalid format to exception message
    (cherry picked from commit 6d81bf17f78b984388693a730e937933e7b2fd35)
    Co-authored-by: Petr Dvořák <petr@wultra.com>

---
+++ powerauth-java-crypto/src/main/java/io/getlime/security/powerauth/crypto/lib/config/Base64SignatureConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * PowerAuth Crypto Library
+ * Copyright 2023 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.getlime.security.powerauth.crypto.lib.config;
+
+import io.getlime.security.powerauth.crypto.lib.enums.PowerAuthSignatureFormat;
+
+/**
+ * Configuration for Base64 signatures.
+ *
+ * @author Petr Dvorak, petr@wultra.com
+ */
+public class Base64SignatureConfiguration extends SignatureConfiguration {
+
+    /**
+     * Constructor with the base64 signature. Package scoped.
+     */
+    Base64SignatureConfiguration() {
+        super(PowerAuthSignatureFormat.BASE64);
+    }
+
+}
