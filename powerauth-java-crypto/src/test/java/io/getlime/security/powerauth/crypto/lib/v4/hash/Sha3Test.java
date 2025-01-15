f0d04bc1e602cdf5f6f355eec513ecf7ec177239
Roman Strobl <roman.strobl@wultra.com>
2025-01-15 10:43:50 +0100

    Reformat code

--- powerauth-java-crypto/src/test/java/io/getlime/security/powerauth/crypto/lib/v4/hash/Sha3Test.java
+++ powerauth-java-crypto/src/test/java/io/getlime/security/powerauth/crypto/lib/v4/hash/Sha3Test.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.getlime.security.powerauth.crypto.lib.v4.hash;
 
 import com.fasterxml.jackson.core.type.TypeReference;
