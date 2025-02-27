18b2388ccefca32279927f63f94850b8cd2adc93
Roman Strobl <roman.strobl@wultra.com>
2025-02-27 20:33:42 +0100

    Update JavaDoc

--- powerauth-java-crypto/src/main/java/com/wultra/security/powerauth/crypto/server/util/DataDigest.java
+++ powerauth-java-crypto/src/main/java/com/wultra/security/powerauth/crypto/server/util/DataDigest.java
@@ -104,8 +104,6 @@ public class DataDigest {
      * Data digest of the list with string elements. Data is first normalized (items concatenated
      * by '&amp;' character), then a random key is generated and hash (HMAC-SHA256) is computed. Finally,
      * the resulting MAC is decimalized to the signature of a length 8 numeric digits.
-     * <p>
-     * In case the digest could not be computed, null value is returned.
      *
      * @deprecated use {@link #generateDigest(String, List)}
      *
@@ -122,8 +120,6 @@ public class DataDigest {
      * Data digest of the list with string elements. Data is first normalized (items concatenated
      * by '&amp;' character), then a random key is generated and hash (HMAC-SHA256) is computed. Finally,
      * the resulting MAC is decimalized to the signature of a length 8 numeric digits.
-     * <p>
-     * In case the digest could not be computed, null value is returned.
      *
      * @param version Cryptography protocol version.
      * @param items Items to be serialized into digest.
