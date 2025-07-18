89799e0fe9eadd8970ef1f1506b4e99e3f5e140a
Juraj Ďurech <1719814+hvge@users.noreply.github.com>
2025-07-18 10:39:45 +0200

    Update shared secret test (#763)

--- powerauth-java-crypto/src/test/java/com/wultra/security/powerauth/crypto/lib/v4/sharedsecret/SharedSecretHybridTest.java
+++ powerauth-java-crypto/src/test/java/com/wultra/security/powerauth/crypto/lib/v4/sharedsecret/SharedSecretHybridTest.java
@@ -27,6 +27,9 @@ import com.wultra.security.powerauth.crypto.lib.v4.model.request.RequestCryptogr
 import com.wultra.security.powerauth.crypto.lib.v4.model.request.SharedSecretRequestHybrid;
 import com.wultra.security.powerauth.crypto.lib.v4.model.response.ResponseCryptogram;
 import com.wultra.security.powerauth.crypto.lib.v4.model.response.SharedSecretResponseHybrid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -35,8 +38,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.PrivateKey;
-import java.security.Security;
+import java.security.*;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -83,6 +85,56 @@ public class SharedSecretHybridTest {
                 derivedSharedSecret,
                 serverResponse.getSecretKey()
         );
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class HybridTestEntry {
+        private String clientContext;
+        private SharedSecretRequestHybrid request;
+        private SharedSecretResponseHybrid response;
+        private String sharedSecret;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class HybridTestData {
+        private HybridTestEntry[] testData;
+    }
+
+    @Test
+    public void testHybridFromClient() throws Exception {
+        /*
+         Usage of this test:
+
+         1. In PowerAuthCore project
+            - Open `SharedSecretTests.cpp`
+            - Enable `genTestVectors_EC_P384_ML_L3` test method (uncomment the method in Unit test's constructor)
+            - Run unit tests
+            - Copy result printed in `genTestVectors_EC_P384_ML_L3()` function
+            - Paste test data to `ECDHE_P384_MLKEM_768_Client_Vectors.json` (see below)
+         2. Run this test case
+            - Copy result printed in the test case
+         3. In PowerAuthCore project
+            - Paste result to `ECDHE_P384_MLKEM_768_Client_Vectors.json`
+            - Run script: `src/PowerAuthTests/TestData/update-pa2-files.sh`
+            - Disable `genTestVectors_EC_P384_ML_L3` test method
+            - Run unit test
+         */
+        InputStream stream = SharedSecretHybridTest.class.getResourceAsStream("/com/wultra/security/powerauth/crypto/lib/v4/sharedsecret/ECDHE_P384_MLKEM_768_Client_Vectors.json");
+        assertNotNull(stream);
+        HybridTestData hybridTestData = MAPPER.readValue(stream, new TypeReference<>() {});
+        assertNotNull(hybridTestData);
+
+        SharedSecretHybrid algorithm = new SharedSecretHybrid();
+        for (HybridTestEntry entry : hybridTestData.getTestData()) {
+             final ResponseCryptogram responseCryptogram = algorithm.generateResponseCryptogram(entry.getRequest());
+             entry.setSharedSecret(Base64.getEncoder().encodeToString(responseCryptogram.getSecretKey().getEncoded()));
+             entry.setResponse((SharedSecretResponseHybrid) responseCryptogram.getSharedSecretResponse());
+        }
+        System.out.println(MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(hybridTestData));
     }
 
     private static Stream<Map<String, String>> jsonDataEcdhe_P384_Mlkem_768_Provider() throws IOException {
