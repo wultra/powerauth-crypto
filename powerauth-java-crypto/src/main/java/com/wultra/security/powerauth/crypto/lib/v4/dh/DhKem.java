5d746bf3b71009c2e021ef152887d5d52c1c8dc4
Jan Pešek <jan.pesek@wultra.com>
2025-12-17 12:09:44 +0100

    Fix #857: HPKE object is shared (#858)
    (cherry picked from commit 6d17425b651a36811e1ca2932cb414de3c3bf80c)

--- powerauth-java-crypto/src/main/java/com/wultra/security/powerauth/crypto/lib/v4/dh/DhKem.java
+++ powerauth-java-crypto/src/main/java/com/wultra/security/powerauth/crypto/lib/v4/dh/DhKem.java
@@ -23,7 +23,6 @@ import com.wultra.security.powerauth.crypto.lib.model.exception.CryptoProviderEx
 import com.wultra.security.powerauth.crypto.lib.model.exception.GenericCryptoException;
 import com.wultra.security.powerauth.crypto.lib.util.KeyConvertor;
 import com.wultra.security.powerauth.crypto.lib.v4.api.Kem;
-import com.wultra.security.powerauth.crypto.lib.v4.ml.MlKemKeyConvertor;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.hpke.HPKE;
 import org.bouncycastle.crypto.hpke.HPKEContext;
@@ -63,13 +62,6 @@ public class DhKem implements Kem {
     private static final byte[] INFO = "DHKEM-P384".getBytes(StandardCharsets.UTF_8);
     private static final int SHARED_SECRET_BYTES_LENGTH = 48;
 
-    private final HPKE hpke = new HPKE(
-            HPKE.mode_base,
-            HPKE.kem_P384_SHA384,
-            HPKE.kdf_HKDF_SHA384,
-            HPKE.aead_EXPORT_ONLY
-    );
-
     @Override
     public KeyPair generateKeyPair() throws GenericCryptoException {
         try {
@@ -83,7 +75,7 @@ public class DhKem implements Kem {
     public SecretKeyWithEncapsulation encapsulate(PublicKey encapsulationKey) throws GenericCryptoException {
         try {
             final AsymmetricKeyParameter keyParameterPublic = PublicKeyFactory.createKey(encapsulationKey.getEncoded());
-            final HPKEContextWithEncapsulation senderCtx = hpke.setupBaseS(keyParameterPublic, INFO);
+            final HPKEContextWithEncapsulation senderCtx = createHpkeInstance().setupBaseS(keyParameterPublic, INFO);
             final byte[] sharedSecret = senderCtx.export(INFO, SHARED_SECRET_BYTES_LENGTH);
             final byte[] encapsulation = senderCtx.getEncapsulation();
             final SecretKey secretKey = new SecretKeySpec(sharedSecret, "RAW");
@@ -99,7 +91,7 @@ public class DhKem implements Kem {
             final ECPrivateKeyParameters privParam = (ECPrivateKeyParameters) PrivateKeyFactory.createKey(decapsulationKey.getEncoded());
             final ECPublicKeyParameters pubParam = toPublicKey(privParam);
             final AsymmetricCipherKeyPair receiverKeyPair = new AsymmetricCipherKeyPair(pubParam, privParam);
-            final HPKEContext receiverCtx = hpke.setupBaseR(ciphertext, receiverKeyPair, INFO);
+            final HPKEContext receiverCtx = createHpkeInstance().setupBaseR(ciphertext, receiverKeyPair, INFO);
             final byte[] sharedSecret = receiverCtx.export(INFO, SHARED_SECRET_BYTES_LENGTH);
             return new SecretKeySpec(sharedSecret, "RAW");
         } catch (Exception e) {
@@ -130,6 +122,15 @@ public class DhKem implements Kem {
         final ECDomainParameters domainParam = privParam.getParameters();
         final ECPoint q = domainParam.getG().multiply(privParam.getD());
         return new ECPublicKeyParameters(q, domainParam);
+    }
+
+    private HPKE createHpkeInstance() {
+        return new HPKE(
+                HPKE.mode_base,
+                HPKE.kem_P384_SHA384,
+                HPKE.kdf_HKDF_SHA384,
+                HPKE.aead_EXPORT_ONLY
+        );
     }
 
 }
